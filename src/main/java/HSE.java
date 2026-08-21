679bf12593d682d9cfa607b97fead18fd056f4e2
Shee En <e1522904@u.nus.edu>
2026-08-21 14:36:05 +0800

    Level 1 and 2: Echo commands and add task list

--- src/main/java/HSE.java
+++ src/main/java/HSE.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class HSE {
     public static void main(String[] args) {
         String banner = " _   _  ____  _____ \n"
@@ -6,9 +8,31 @@ public class HSE {
                 + "|  _  | ___) | |___ \n"
                 + "|_| |_||____/|_____|\n";
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println(banner);
         System.out.println("Hello! I'm HSE.");
         System.out.println("What can I do for you?");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String command = scanner.nextLine();
+
+                if (command.equals("bye")) {
+                    break;
+                } else if (command.equals("list")) {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                } else {
+                    tasks[taskCount] = command;
+                    taskCount++;
+                    System.out.println("added: " + command);
+                }
+            }
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
