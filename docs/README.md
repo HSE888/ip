f59888c84630ac24bddc98ed7ce08c0c60f7abaa
Shee En <e1522904@u.nus.edu>
2026-08-21 14:13:48 +0800

    Level 0: Rename chatbot to HSE and implement greet/exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# HSE User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/HSE.java
@@ -0,0 +1,14 @@
+public class HSE {
+    public static void main(String[] args) {
+        String banner = " _   _  ____  _____ \n"
+                + "| | | |/ ___|| ____|\n"
+                + "| |_| |\\___ \\|  _|  \n"
+                + "|  _  | ___) | |___ \n"
+                + "|_| |_||____/|_____|\n";
+
+        System.out.println(banner);
+        System.out.println("Hello! I'm HSE.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
